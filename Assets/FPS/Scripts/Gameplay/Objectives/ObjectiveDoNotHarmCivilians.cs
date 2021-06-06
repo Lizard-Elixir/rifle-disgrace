6e5783eb65af8b66e75dbd3a2d1f7454e3edc303
Alex Van Camp <vancamp.alex@gmail.com>
2021-06-05 20:32:01 -0500

    Shooting a civilian now shows an "Objective failed" message

---
+++ Assets/FPS/Scripts/Gameplay/Objectives/ObjectiveDoNotHarmCivilians.cs
@@ -0,0 +1,35 @@
+﻿using Unity.FPS.Game;
+using UnityEngine;
+
+namespace Unity.FPS.Gameplay
+{
+	public class ObjectiveDoNotHarmCivilians : Objective
+	{
+		protected override void Start()
+		{
+			base.Start();
+
+			EventManager.AddListener<EnemyKillEvent>(OnEnemyKilled);
+
+			// set a title and description specific for this type of objective, if it hasn't one
+			if (string.IsNullOrEmpty(Title))
+				Title = "Do not harm any civilians";
+		}
+
+		void OnEnemyKilled(EnemyKillEvent evt)
+		{
+			if (IsFailed)
+				return;
+
+			if (evt.Enemy.CompareTag("Civilian"))
+			{
+				FailObjective(string.Empty, "Objective failed : " + Title);
+			}
+		}
+
+		void OnDestroy()
+		{
+			EventManager.RemoveListener<EnemyKillEvent>(OnEnemyKilled);
+		}
+	}
+}
